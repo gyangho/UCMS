65a11c222861f20af3ebe47625c8c4b27f467176
gyangho <52252845+gyangho@users.noreply.github.com>
2025-07-05 04:25:47 +0900

    2025.07.05 이경호
    카카오 로그인 구현 - 로그인 사용자 정보 저장 및 개인정보 받기 구현 완료
    https://github.com/gyangho/UCMS/issues/3

--- main.js
+++ main.js
@@ -1,29 +1,76 @@
-const express = require('express');
-const session = require('express-session');
-const bodyParser = require('body-parser');
-const db = require('./db');
-const axios = require('axios');
+require("dotenv-expand").expand(require("dotenv").config());
 
-const defaultRouter = require('./routes/router');
-const apiRouter = require('./routes/api');
-const loginRouter = require('./routes/login');
+const express = require("express");
+const session = require("express-session");
+const mySQLSessionStore = require("express-mysql-session")(session);
+
+const db = require("./db");
+const defaultRouter = require("./routes/router");
+const apiRouter = require("./routes/api");
+const authRouter = require("./routes/auth");
 
 const app = express();
-const PORT = 3000;
+const DOMAIN = process.env.DOMAIN;
+const PORT = process.env.PORT;
+const sessionStore = new mySQLSessionStore(
+  {
+    clearExpired: true,
+    checkExpirationInterval: 600000, // 10분마다 정리
+    expiration: 86400000, // 세션 만료는 24시간
+  },
+  db
+);
 
-app.use(session({
-    secret: 'secert',
+function requireValidSession(req, res, next) {
+  if (!req.sessionID) {
+    if (req.path === "/") {
+      return next(); // 로그인 전 메인 페이지 접근 허용
+    }
+    return res.redirect("/");
+  }
+
+  sessionStore.get(req.sessionID, (err, session, next) => {
+    if (!session) {
+      if (req.path === "/") {
+        return next();
+      }
+      return res.redirect("/");
+    } else if (err) {
+      return next(err);
+    }
+    if (req.path === "/") {
+      return res.redirect("/dashboard");
+    }
+    return next();
+  });
+}
+
+app.use(
+  session({
+    secret: "secert",
     resave: false,
-    saveUninitialized: false
-}));
+    saveUninitialized: false,
+    store: sessionStore,
+    cookie: { maxAge: 1000 * 60 * 60 },
+  })
+);
 
-app.use(bodyParser.json());
+app.use("/", defaultRouter);
+app.use("/api", apiRouter);
+app.use("/auth", authRouter);
 
-app.use('/', defaultRouter);
-app.use('/api', apiRouter);
-app.use('/auth', loginRouter);
+app.use(requireValidSession);
 
-app.listen(PORT, () => {
-    console.log(`Server is running on \"http://localhost:${PORT}\"`);
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.send(`
+    <script>
+      alert("Internal Server ERROR");
+      window.location.href = "/auth/logout"; 
+    </script>
+  `);
 });
 
+app.listen(PORT, () => {
+  console.log(`Server is running on \"${DOMAIN}\"`);
+});
